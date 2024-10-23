ed3696c2f728d7cb49e745e325c9f589b90f4576
Exaze TRSA27 <Exaze TRSA27@EXZ_MP2HMP1P>
2024-10-23 11:07:14 +0200

    Add project files.

---
+++ LeaveManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of Onthatile",
+                DateOfBirth = new DateTime(2001,11,06)
+
+            };
+
+            return View(data);
+        }
+    }
+}
